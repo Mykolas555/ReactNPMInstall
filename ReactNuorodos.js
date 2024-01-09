f293edbb6cf65aa55e60aaccfc53a114659cc459
Mykolas555 <145454415+Mykolas555@users.noreply.github.com>
2024-01-09 11:58:32 +0200

    Add files via upload

---
+++ ReactNuorodos.js
@@ -0,0 +1,19 @@
+//REACT kurimas
+npx create-react-app my-app
+npm start
+
+//BOOTSTRAP
+npm install react-bootstrap bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+npm install react-bootstrap-icons
+
+//SASS
+npm install sass
+
+//ROUTER
+npm i react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//FIREBASE
+npm install firebase
+npm i react-firebase-hooks
